7b9c239e71be0a86d0e9bf66cbea4e9fe4e38119
LoneRifle <LoneRifle@users.noreply.github.com>
2019-01-25 18:18:42 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,114 @@
+const API_KEY = 'keyhlCvW8cKeIALjf'
+const INCOMING_ITEMS = 'https://api.airtable.com/v0/appK5r1OQWwkZ1t6L/Incoming%20Items'
+const INVENTORY = 'https://api.airtable.com/v0/appK5r1OQWwkZ1t6L/Inventory'
+
+function log (text) {
+  $('#status').text(text)
+}
+
+function toggleProductFields (disabled) {
+  $('#product-details input[type=text]').prop('readonly', disabled)
+  $('#product-details input[type=checkbox]').prop('disabled', disabled)
+  $('#product-details select').prop('disabled', disabled)
+}
+
+async function registerNewProduct () {
+  const record = { fields: {} }
+  $('form').serializeArray()
+    .filter(v => ['Description', 'Size', 'Category'].includes(v.name))
+    .forEach(v => record.fields[v.name] = v.value)
+  record.fields['Halal?'] = $('input[name=Halal\\?]').prop('checked')
+  record.fields.UOM = $('select[name=UOM]').val()
+  record.fields.Barcode = { text: $('input[name=Barcode]').val() }
+
+  const post = $.post({
+    url: `${INVENTORY}?api_key=${API_KEY}`, 
+    data: JSON.stringify(record),
+    success: data => {
+      log('Registered new product!')
+      console.log('Registered new product:', data)
+      $('input[name=Product]').val(data.id)
+      toggleProductFields(true)
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+  })
+
+  return post.promise()
+}
+
+function addIncomingItem () {
+  const record = { fields: {} }
+  $('form').serializeArray()
+    .filter(v => ['Product', 'Quantity'].includes(v.name))
+    .forEach(v => record.fields[v.name] = v.value)
+  record.fields.Product = [record.fields.Product]
+  record.fields.Quantity = Number(record.fields.Quantity)
+  $.post({
+    url: `${INCOMING_ITEMS}?api_key=${API_KEY}`, 
+    data: JSON.stringify(record),
+    success: data => {
+      log('Item added!')
+      console.log('Added to incoming items:', data)
+      $('input[name=Product]').val(undefined)
+      $('form').trigger('reset')
+    },
+    dataType: 'json',
+    contentType: 'application/json',
+  })
+}
+
+function go () {
+  $('#scan').click(async () => {
+    const codeReader = new ZXing.BrowserBarcodeReader()
+
+    codeReader
+      .decodeFromInputVideoDevice(undefined, 'video')
+      .then(result => {
+        $('input[name=Barcode]').val(result.text)
+        $('#lookup').trigger('click')
+        codeReader.reset()
+      })
+      .catch(err => console.error(err))
+  })
+
+
+  $('#lookup').click(() => {
+    $('input[name=Product]').val(undefined)
+    $('#product-details input').val(undefined)
+    $('select[name=UOM]').val('')
+    $('input[name=Halal\\?]').prop('checked', false)
+
+    const barcode = $('input[name=Barcode]').val()
+    $.get({
+      url: `${INVENTORY}?api_key=${API_KEY}&filterByFormula=${encodeURIComponent('{ID}='+barcode)}`,
+      success: data => {
+        const [record] = data.records
+        if (record) {
+          $('input[name=Product]').val(record.id)
+          ;['Description', 'Size', 'Category'].forEach(field => {
+            $(`input[name=${field}]`).val(record.fields[field])
+          })
+          $('select[name=UOM]').val(record.fields.UOM)
+          $('input[name=Halal\\?]').prop('checked', record.fields['Halal?'])
+        } else {
+          log('This is a new product =(')
+          toggleProductFields(false)
+        }
+      },
+      dataType: 'json',
+      contentType: 'application/json',
+    })
+  })
+
+
+  $('form').on('submit', async e => {
+    e.preventDefault()
+    if ($('input[name=Product]').val() === '') {
+      await registerNewProduct()
+    }
+    addIncomingItem()
+  })
+}
+
+$(document).ready(go)
